4902a9a2f31b1d648654cb23daf388e84f0582d5
qalqaa <113594321+qalqaa@users.noreply.github.com>
2025-01-19 08:36:02 +0300

    chore: some clean up + simple app sctructure added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer.tsx';
+import Header from './components/Header/Header.tsx';
+import './index.css';
+import App from './pages/App/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Header />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </Router>
+    <Footer />
   </StrictMode>,
-)
+);
---
+++ src/pages/App/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <main className={styles.container}>
+      <h1>qalqa's Kitty Pinterest</h1>
+    </main>
+  );
+}
+
+export default App;
