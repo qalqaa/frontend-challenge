0a90989dd5b565681cb745c49ba212c81fdff170
qalqaa <113594321+qalqaa@users.noreply.github.com>
2025-01-20 08:59:36 +0300

    feat: added burger

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useCatStore } from '../../stores/catStore';
 import { ROUTES } from '../../utils/routes';
@@ -5,31 +7,46 @@ import styles from './Header.module.scss';
 
 const Header = () => {
   const { isNeedToAuth } = useCatStore();
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <Link className={styles.logo} to={ROUTES.HOME}>
           <i>🐈</i> Kitenest
         </Link>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <Link className={styles.link} to={ROUTES.HOME}>
-              Все котики
-            </Link>
-          </li>
-          <li className={styles.item}>
-            <Link className={styles.link} to={ROUTES.FAVORITES}>
-              Любимые котики
-            </Link>
-          </li>
-          {isNeedToAuth && (
+        <nav className={styles.nav}>
+          <button
+            className={clsx(styles.burger, { [styles.active]: isMenuOpen })}
+            aria-label="Бургер меню"
+            onClick={toggleMenu}
+          >
+            <span />
+            <span />
+            <span />
+          </button>
+          <ul className={`${styles.list} ${isMenuOpen ? styles.listOpen : ''}`}>
             <li className={styles.item}>
-              <Link className={styles.link} to={ROUTES.AUTH}>
-                Войти
+              <Link className={styles.link} to={ROUTES.HOME}>
+                Все котики
               </Link>
             </li>
-          )}
-        </ul>
+            <li className={styles.item}>
+              <Link className={styles.link} to={ROUTES.FAVORITES}>
+                Любимые котики
+              </Link>
+            </li>
+            {isNeedToAuth && (
+              <li className={styles.item}>
+                <Link className={styles.link} to={ROUTES.AUTH}>
+                  Войти
+                </Link>
+              </li>
+            )}
+          </ul>
+        </nav>
       </div>
     </header>
   );
